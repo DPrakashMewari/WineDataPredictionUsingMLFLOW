10c55c3fad8529eb126c575a64aa6fcb77597b89
Chandra Prakash <95275695+DPrakashM@users.noreply.github.com>
2023-08-04 12:19:44 +0530

    Data Transformation

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from src.ml_project import logger
 from src.ml_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.ml_project.pipeline.stage_02_data_validaton import DataValidationTrainingPipeline
+from src.ml_project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
-STAGE_NAME = "Data Ingestion Stage"
-STAGE_NAME = "Data Validation Stage"
+
+STAGE_NAME1 = "Data Ingestion Stage"
+STAGE_NAME2 = "Data Validation Stage"
+STAGE_NAME3 = "Data Transformation Stage"
 try:
-    logger.info(f"<<<<<{STAGE_NAME}>>>>>")
+    logger.info(f"<<<<<{STAGE_NAME1}>>>>>")
     obj = DataIngestionTrainingPipeline()
     obj.main()
-    logger.info(f"<<<<{STAGE_NAME} completed>>>")
+    logger.info(f"<<<<{STAGE_NAME1} completed>>>")
 except Exception as e:
     logger.warning(f"Exception Occur{e}")
     raise e
@@ -16,10 +19,23 @@ except Exception as e:
 
 
 try:
-    logger.info(f"<<<<<{STAGE_NAME}>>>>>")
+    logger.info(f"<<<<<{STAGE_NAME2}>>>>>")
     obj = DataValidationTrainingPipeline()
     obj.main()
-    logger.info(f"<<<<{STAGE_NAME} completed>>>")
+    logger.info(f"<<<<{STAGE_NAME2} completed>>>")
 except Exception as e:
     logger.warning(f"Exception Occur{e}")
     raise e
+
+
+
+try:
+    logger.info(f"<<<<<{STAGE_NAME3}>>>>>")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f"<<<<{STAGE_NAME3} completed>>>")
+except Exception as e:
+    logger.warning(f"Exception Occur{e}")
+    raise e
+
+
--- src/ml_project/config/configuration.py
+++ src/ml_project/config/configuration.py
@@ -1,6 +1,6 @@
 from src.ml_project.constants import * 
 from src.ml_project.utils.common import read_yaml,create_directories
-from src.ml_project.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from src.ml_project.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 
 class ConfigurationManger:
     def __init__(self,config_filepath = CONFIG_FILE_PATH,params_filepath = PARAMS_FILE_PATH,schema_filepath = SCHEMA_FILE_PATH):
@@ -46,6 +46,19 @@ class ConfigurationManger:
         )
 
         return data_validation_config
-    
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config    
+
+
 
 
